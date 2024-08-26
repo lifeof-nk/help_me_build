3bcbe617a4a4059b3d85f70f9e3ed5c8ceb68fec
Frederick Arthur <69360854+lifeof-nk@users.noreply.github.com>
2024-08-26 04:39:06 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Does this work?</h1>
+      <h1>i hope this actually works</h1>
       <p>Hopefully i get a hang of this thing and master it</p>
     </>
   );
